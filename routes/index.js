6a109184b77f46cfdf496bd99d613d47cd7d83c7
Jason Bellamy <j@sonbellamy.com>
2015-11-30 13:10:49 -0500

    Remove / route

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const router  = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.status(200).send({ success: true });
-});
-
 router.use('/api/v1', require('./v1/index'));
 
 router.use((req, res, next) => {
