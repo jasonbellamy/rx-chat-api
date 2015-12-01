a1279a8477af9f64673dadfd23803ae73b0220a7
Jason Bellamy <j@sonbellamy.com>
2015-12-01 17:21:27 -0500

    Cleanup route path

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router  = express.Router();
 
-router.use('/api/v1', require('./v1/index'));
+router.use('/api/v1', require('./v1/'));
 
 router.use((req, res, next) => {
   res.status(404).send({ success: true });
