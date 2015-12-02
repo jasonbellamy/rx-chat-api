4baf5a3e63b29bffa61bc3fb932bf395c34b4c9c
Jason Bellamy <j@sonbellamy.com>
2015-12-02 17:07:21 -0500

    Refactor app

--- app.js
+++ app.js
@@ -5,17 +5,17 @@ import bodyParser from 'body-parser';
 import config from './config/';
 import routes from './routes/';
 
-const app = express();
+export default (function() {
+  const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+  if (app.get('env') === 'development') {
+    mongoose.connect(config.database);
+  }
 
-if (app.get('env') === 'development') {
-  mongoose.connect(config.database);
-}
+  return app
+    .use(bodyParser.json())
+    .use(bodyParser.urlencoded({ extended: false }))
+    .use('/', routes())
+    .listen(config.port);
 
-app.use('/', routes());
-
-app.listen(config.port);
-
-export default app;
+})();
