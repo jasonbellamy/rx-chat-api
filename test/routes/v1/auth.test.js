03de2353ab2310f1a471d882ff24d081929fa29d
Jason Bellamy <j@sonbellamy.com>
2015-12-02 11:19:59 -0500

    Add basic login auth error/success tests

--- test/routes/v1/auth.test.js
+++ test/routes/v1/auth.test.js
@@ -1,14 +1,29 @@
 const assert  = require('chai').assert;
 const request = require('supertest');
 const app     = require('../../../app');
+const User    = require('../../../models/user');
 
 describe('Routes: API:Auth (V1)', () => {
   describe('/login', () => {
-    it('should return a 200 "OK" request', (done) => {
+
+    before(done => {
+      User.create({username: 'username', password: 'password'}, () => done());
+    });
+
+    it('should return a 200 "OK" request if login is successful', (done) => {
       request(app)
       .post("/api/v1/auth/login")
+      .send({ username: 'username', password: 'password' })
       .expect("Content-type",/json/)
       .expect(200, done);
+    });
+
+    it('should return a 401 "Unauthorized" request if login failed', (done) => {
+      request(app)
+      .post("/api/v1/auth/login")
+      .send({ username: 'badusername', password: 'password' })
+      .expect("Content-type",/json/)
+      .expect(401, done);
     });
   });
 
