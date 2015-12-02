9d3fc7b40bf8953f37e51abd89dc1a935f124fa9
Jason Bellamy <j@sonbellamy.com>
2015-12-02 11:20:30 -0500

    Add functionality to let user to auth

--- routes/v1/auth.js
+++ routes/v1/auth.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const router  = express.Router();
+const User    = require('../../models/user');
 
 router.post('/login', (req, res, next) => {
-  res.status(200).send({ success: true });
+
+  User.findOne({ username: req.body.username }, (err, user) => {
+    if (!user) {
+      return res.status(401).send({ success: false, message: 'Authentication failed. User not found.' });
+    }
+
+    if (user.password !== req.body.password) {
+      return res.status(401).send({ success: false, message: 'Authentication failed. Wrong password.' });
+    }
+
+    res.status(200).send({ success: true });
+  });
 });
 
 router.post('/logout', (req, res, next) => {
