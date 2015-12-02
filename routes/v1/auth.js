115f2920555887fb65a045458adff36f3f624155
Jason Bellamy <j@sonbellamy.com>
2015-12-02 13:25:54 -0500

    Add message to success

--- routes/v1/auth.js
+++ routes/v1/auth.js
@@ -3,8 +3,11 @@ const router  = express.Router();
 const User    = require('../../models/user');
 
 router.post('/login', (req, res, next) => {
-
   User.findOne({ username: req.body.username }, (err, user) => {
+    if (err) {
+      return res.status(401).send({ success: false, message: 'Authentication failed.' });
+    }
+
     if (!user) {
       return res.status(401).send({ success: false, message: 'Authentication failed. User not found.' });
     }
@@ -12,8 +15,7 @@ router.post('/login', (req, res, next) => {
     if (user.password !== req.body.password) {
       return res.status(401).send({ success: false, message: 'Authentication failed. Wrong password.' });
     }
-
-    res.status(200).send({ success: true });
+    res.status(200).send({ success: true, message: 'Authentication successful' });
   });
 });
 
