345333580cdb70f56fb836dcc3997d5163801b3a
Jason Bellamy <j@sonbellamy.com>
2015-12-03 15:57:39 -0500

    Create & return a token on user login

--- routes/v1/auth.js
+++ routes/v1/auth.js
@@ -1,5 +1,7 @@
+import config from '../../config';
 import { Router } from 'express';
 import { successResponse, errorResponse } from '../../lib/responses';
+import jwt from 'jsonwebtoken';
 import User from '../../models/user';
 
 export default function() {
@@ -20,7 +22,9 @@ export default function() {
         return res.status(401).json(errorResponse('Authentication failed. Wrong password'));
       }
 
-      return res.status(200).json(successResponse('Authentication successful'));
+      const token = jwt.sign(user, config.secret, { expiresIn: 86400 });
+
+      return res.status(200).json(successResponse('Authentication successful', { token }));
     });
   });
 
--- test/routes/v1/auth.test.js
+++ test/routes/v1/auth.test.js
@@ -1,15 +1,17 @@
 import { assert } from 'chai';
 import request from 'supertest';
+import jwt from 'jsonwebtoken';
 import app from '../../../app';
+import config from '../../../config';
 import User from '../../../models/user';
 
 describe('Routes: API:Auth (V1)', () => {
   describe('/login', () => {
-    before(done => {
+    beforeEach(done => {
       User.create({username: 'username', password: 'password'}, () => done());
     });
 
-    it('should return a 200 "OK" request if login is successful', (done) => {
+    it('should return a 200 "OK" request if the login is successful', (done) => {
       request(app)
       .post("/api/v1/auth/login")
       .send({ username: 'username', password: 'password' })
@@ -17,12 +19,36 @@ describe('Routes: API:Auth (V1)', () => {
       .expect(200, done);
     });
 
-    it('should return a 401 "Unauthorized" request if login failed', (done) => {
+    it('should return a token if the login is successful', (done) => {
+      request(app)
+      .post("/api/v1/auth/login")
+      .send({ username: 'username', password: 'password' })
+      .expect("Content-type",/json/)
+      .expect(200)
+      .end((err, res) => {
+        assert.property(res.body.entities, 'token');
+        done();
+      });
+    });
+
+    it('should return a 401 "Unauthorized" request if the login failed', (done) => {
       request(app)
       .post("/api/v1/auth/login")
       .send({ username: 'badusername', password: 'password' })
       .expect("Content-type",/json/)
       .expect(401, done);
+    });
+
+    it('should not return a token if the login failed', (done) => {
+      request(app)
+      .post("/api/v1/auth/login")
+      .send({ username: 'badusername', password: 'password' })
+      .expect("Content-type",/json/)
+      .expect(401)
+      .end((err, res) => {
+        assert.notProperty(res.body.entities, 'token');
+        done();
+      });
     });
   });
 });
